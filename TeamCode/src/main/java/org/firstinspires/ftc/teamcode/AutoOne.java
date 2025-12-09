17732f2f53692a39e0c3a1642859fe87cd5a2ec1
dorin <dorinamevans@gmail.com>
2025-12-08 16:16:47 -0800

    RoadRunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOne.java
@@ -10,4 +10,22 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotc
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import java.util.Locale;
+import java.util.Map;
+
+@Autonomous
+public class AutoOne extends BaseCodeV2{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initOpMode(true, true, true, true, true);
+        waitForStart();
+        while (opModeIsActive()) {
+
+            telemetry.addLine("Stoped shooting now");
+            shoot();
+        }
+    }
+}
