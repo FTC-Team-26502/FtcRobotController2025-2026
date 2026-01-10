e6018069bcea723de8207aada8896d62003d3644
dorin <dorinamevans@gmail.com>
2026-01-10 13:52:25 -0800

    Auto WOrking maybe

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turningToDepotTest.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+public class turningToDepotTest extends FTC26502OpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
